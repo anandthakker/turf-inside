bad459f4578a420c91071d30faea7aff5ae2550b
Anand Thakker <vestibule@anandthakker.net>
2015-07-30 17:31:05 -0400

    Include boundaries.

--- index.js
+++ index.js
@@ -96,9 +96,33 @@ function inRing (pt, ring) {
   for (var i = 0, j = ring.length - 1; i < ring.length; j = i++) {
     var xi = ring[i][0], yi = ring[i][1];
     var xj = ring[j][0], yj = ring[j][1];
-    var intersect = ((yi > pt[1]) !== (yj > pt[1])) &&
-        (pt[0] < (xj - xi) * (pt[1] - yi) / (yj - yi) + xi);
-    if (intersect) isInside = !isInside;
+    var maxx = xi, minx = xj, maxy = yi, miny = yj;
+    if (xi < xj) { maxx = xj; minx = xi; }
+    if (yi < yj) { maxy = yj; miny = yi; }
+    if (yi === yj && yi === pt[1] && maxx >= pt[0] && minx <= pt[0]) {
+      // if we're on a horizontal segment, return true.
+      return true;
+    }
+    if (maxy < pt[1] || miny > pt[1]) {
+      // if we're not in the same y-range as the segment move on.
+      continue
+    }
+    // find the x-coordinate where the line y=pt[1] intersects this segment
+    var intersect = (xj - xi) * (pt[1] - yi) / (yj - yi) + xi
+    if (pt[0] === intersect) {
+      // if we're on the segment, return true
+      return true
+    }
+    if (pt[0] < intersect) {
+      // if we're to the left of the segment, flip isInside
+      isInside = !isInside;
+      if (yi === pt[1]) {
+        // if this we intersected the leading endpoint, skip the next segment 
+        // so that we don't double count the intersection.
+        i++;
+      }
+    }
   }
+  // represents the parity of edges crossed by the ray (pt[0],pt[1]) -> (infinity, pt[1])
   return isInside;
 }
